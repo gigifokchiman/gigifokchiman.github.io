d4b27d3ced9628b5cd615d4c860288ee58621e07
gigifokchiman <gigifokcm@gmail.com>
2024-02-01 16:52:14 -0800

    Change the FeaturedPost.tsx to my profile info with social and photo

--- src/FeaturedPost.tsx
+++ src/FeaturedPost.tsx
@@ -5,34 +5,61 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
 
 interface FeaturedPostProps {
   post: {
-    date: string;
+    date?: string;
     description: string;
     image: string;
     imageLabel: string;
     title: string;
+    social: ReadonlyArray<{
+      icon: React.ElementType;
+      name: string;
+      url: string;
+    }>;
   };
 }
+
+
+
 
 export default function FeaturedPost(props: FeaturedPostProps) {
   const { post } = props;
 
   return (
-    <Grid item xs={12} md={6}>
+    <Grid item xs={12} md={12}>
       <CardActionArea component="a" href="#">
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
             <Typography component="h2" variant="h5">
               {post.title}
             </Typography>
+            {post.date == null && (
             <Typography variant="subtitle1" color="text.secondary">
               {post.date}
             </Typography>
+            )}
             <Typography variant="subtitle1" paragraph>
               {post.description}
             </Typography>
+            {post.social.map((network) => (
+                <Link
+                    display="block"
+                    variant="body1"
+                    href={network.url}
+                    target="_blank"
+                    key={network.name}
+                    sx={{ mb: 0.5 }}
+                >
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    <network.icon />
+                    <span>{network.name}</span>
+                  </Stack>
+                </Link>
+            ))}
             {/*<Typography variant="subtitle1" color="primary">*/}
             {/*  Continue reading...*/}
             {/*</Typography>*/}
