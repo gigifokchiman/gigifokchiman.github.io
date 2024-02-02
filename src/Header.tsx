a4bf8b63b5842af99d572ee4b0d7e60edf070b1a
gigifokchiman <gigifokcm@gmail.com>
2024-02-01 18:27:54 -0800

    Clean up readme

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+// import IconButton from '@mui/material/IconButton';
+// import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
@@ -27,35 +27,34 @@ export default function Header(props: HeaderProps) {
             color="inherit"
             align="left"
             noWrap
-            sx={{ flex: 1 }}
+            sx={{ flex: 8 }}
           >
           {title}
          </Typography>
         </a>
-          {/*TODO: enable search*/}
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-      </Toolbar>
-      {sections.length >= 1 && ( // Add this line
-      <Toolbar
-        component="nav"
-        variant="dense"
-        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
-        {sections.map((section) => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            sx={{ p: 1, flexShrink: 0 }}
+        {sections.length >= 1 && ( // Add this line
+          <Toolbar
+              component="nav"
+              variant="dense"
+              sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
           >
-            {section.title}
-          </Link>
-        ))}
-      </Toolbar>)}
+            {sections.map((section) => (
+                <Link
+                    color="inherit"
+                    noWrap
+                    key={section.title}
+                    variant="body2"
+                    href={section.url}
+                    sx={{ p: 1, flexShrink: 0 }}
+                >
+                  {section.title}
+                </Link>
+            ))}
+            </Toolbar>)}
+        {/*<IconButton>*/}
+        {/*  <SearchIcon />*/}
+        {/*</IconButton>*/}
+      </Toolbar>
     </React.Fragment>
   );
 }
